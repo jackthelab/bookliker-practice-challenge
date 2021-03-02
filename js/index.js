8b3e509f3c84646385581ce0d0636b16a10bfdd1
Jacob Hampton <jhampton792@gmail.com>
2021-03-02 13:38:05 -0600

    Added show page for book when the title is clicked on

--- js/index.js
+++ js/index.js
@@ -29,5 +29,43 @@ function makeBookList(books){
 }
 
 function showBook(book){
+    const showPanel = document.getElementById('show-panel')
+    showPanel.innerHTML = ''
+
+    const bookCover = document.createElement('img')
+        bookCover.src = book.img_url
     
+    const bookTitle = document.createElement('h2')
+        bookTitle.innerText = book.title
+    
+    const booksubtitle = document.createElement('h3')
+        if(book.subtitle){
+            booksubtitle.innerText = book.subtitle
+        }else{
+            booksubtitle.innerText = ""
+        }
+    
+    const bookAuthor = document.createElement('h3')
+        bookAuthor.innerText = book.author
+    
+    const bookDescription = document.createElement('p')
+        bookDescription.innerText = book.description
+
+    const likesList = document.createElement('ul')
+        book.users.forEach(user => {
+            const fan = document.createElement('li')
+                fan.innerText = user.username
+            likesList.appendChild(fan)
+        })
+    
+    const likeBtn = document.createElement('button')
+        likeBtn.innerText = "Like"
+
+    showPanel.append(bookCover, bookTitle)
+
+    if(booksubtitle !== ""){
+        showPanel.append(booksubtitle)
+    }
+
+    showPanel.append(bookAuthor, bookDescription, likesList, likeBtn)
 }
