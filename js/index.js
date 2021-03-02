4dcd2bb374427329758442037194c719268dd46c
Jacob Hampton <jhampton792@gmail.com>
2021-03-02 13:24:08 -0600

    Added List of Books

--- js/index.js
+++ js/index.js
@@ -1,1 +1,33 @@
-document.addEventListener("DOMContentLoaded", function() {});
+BASE_URL = 'http://localhost:3000/'
+BOOKS_URL = BASE_URL+'books/'
+USERS_URL = BASE_URL+'users/'
+
+document.addEventListener("DOMContentLoaded", function() {
+    fetchBooks()
+});
+
+function fetchBooks() {
+    fetch(BOOKS_URL)
+        .then(res => res.json())
+        .then(booksData => {
+            makeBookList(booksData)
+        })
+}
+
+function makeBookList(books){
+    books.forEach(book => {
+        const bookList = document.getElementById('list')
+
+        const bookListItem = document.createElement('li')
+            bookListItem.innerText = book.title
+            bookListItem.addEventListener('click', () => {
+                showBook(book)
+            })
+        
+        bookList.append(bookListItem)
+    })
+}
+
+function showBook(book){
+    
+}
